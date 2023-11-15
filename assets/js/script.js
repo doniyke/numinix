531f6682c9eebce2f1ad5aa77b0b7385b1cb4de2
Davis <iykedavis69@gmail.com>
2023-11-15 01:50:23 +0100

    Updated margin, padding and spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ function displayUsers(userArray) {
     if (userArray.length > 0) {
         userArray.forEach((user) => {
             const div = document.createElement('div');
-            div.classList.add('col-md-3', 'mb-4');
+            div.classList.add('col-lg-3','col-md-6','col-sm-6', 'profile-data-holder');
             let templateString = `
             
                 <div class="profile-data">
@@ -34,7 +34,7 @@ function displayUsers(userArray) {
                         <div class="d-block d-md-none">
                             <div class="row">
                                 <div class="col-8">
-                                    <div class="p-2">
+                                    <div class="t-data">
                                         <h3>${user.name}</h3>
                                         <h4>@${user.username}</h4>
                                         <p>
@@ -53,7 +53,7 @@ function displayUsers(userArray) {
                             <div class="img-holder">
                                 <img src="https://loremflickr.com/500/500/man?random=${user.id}" class="img-fluid">
                             </div>
-                            <div class="p-2 mb-0 pb-0">
+                            <div class="mb-0 pb-0 t-data">
                                 <h3>${user.name}</h3>
                                 <h4>@${user.username}</h4>
                                 <p>
@@ -62,7 +62,7 @@ function displayUsers(userArray) {
                             </div>
                         </div>
                     </div>
-                    <div class="bottom-data p-2 mt-0 pt-0">
+                    <div class="bottom-data mt-0 pt-0">
                         <div class="d-flex">
                             <p class="gap">
                                 <span>
